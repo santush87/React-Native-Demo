4cc9ad4098b0109cd19e0492d43025008cb2a552
Martin Aleksandrov <martin.aleksandrov87@gmail.com>
2024-01-08 11:07:55 +0200

    Remove Goal Input functionality from App.js to GoalInput.js

---
+++ DailyGoals/components/GoalInput.js
@@ -0,0 +1,49 @@
+import { useState } from "react";
+import { View, TextInput, Button, StyleSheet } from "react-native";
+
+function GoalInput(props) {
+  const [enteredGoalText, setEnteredGoalText] = useState("");
+
+  function goalInputHandler(enteredText) {
+    setEnteredGoalText(enteredText);
+  }
+
+  function addGoalHandler() {
+    props.onAddGoal(enteredGoalText);
+    setEnteredGoalText("");
+  }
+
+  return (
+    <View style={styles.inputContainer}>
+      <TextInput
+        placeholder="Your course goal!"
+        style={styles.textInput}
+        onChangeText={goalInputHandler}
+        value={enteredGoalText}
+      />
+      <Button title="Add Goal" onPress={addGoalHandler} />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginBottom: 24,
+    borderBottomWidth: 1,
+    borderBottomColor: "#cccccc",
+  },
+  textInput: {
+    borderWidth: 1,
+    borderRadius: 50,
+    borderColor: "#cccccc",
+    width: "70%",
+    marginRight: 8,
+    padding: 8,
+  },
+});
+
+export default GoalInput;
