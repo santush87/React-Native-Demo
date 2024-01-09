24e8c83f79bd38b7c1d349c1bddf60ecfa310782
Martin Aleksandrov <martin.aleksandrov87@gmail.com>
2024-01-09 11:39:42 +0200

    Created custom buttons

---
+++ GuessTheNumber/components/PrimaryButton.js
@@ -0,0 +1,48 @@
+import { View, Text, Pressable, StyleSheet } from "react-native";
+
+function PrimaryButton({ children }) {
+  function handler() {
+    console.log("Pressed!");
+  }
+  return (
+    <View style={styles.buttonOuterContainer}>
+      <Pressable
+        style={({ pressed }) =>
+          pressed
+            ? [styles.buttonInnerContainer, styles.pressed]
+            : styles.buttonInnerContainer
+        }
+        onPress={handler}
+        // Only for Android - an effect!
+        android_ripple={{ color: "640233" }}
+      >
+        <Text style={styles.buttonText}>{children}</Text>
+      </Pressable>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  buttonOuterContainer: {
+    borderRadius: 28,
+    margin: 4,
+    overflow: "hidden",
+  },
+  buttonInnerContainer: {
+    backgroundColor: "#72063c",
+    paddingVertical: 8,
+    paddingHorizontal: 16,
+    // Only for Android
+    elevation: 2,
+  },
+  buttonText: {
+    color: "white",
+    textAlign: "center",
+  },
+  // Only for iOS
+  pressed: {
+    opacity: 0.75,
+  },
+});
+
+export default PrimaryButton;
---
+++ GuessTheNumber/screens/StartGameScreen.js
@@ -0,0 +1,48 @@
+import { View, TextInput, StyleSheet } from "react-native";
+import PrimaryButton from "../components/PrimaryButton";
+
+function StartGameScreen() {
+  return (
+    <View style={styles.inputContainer}>
+      <TextInput
+        style={styles.nameInput}
+        maxLength={2}
+        keyboardType="number-pad"
+        autoCapitalize="none"
+        autoCorrect={false}
+      />
+      <PrimaryButton>Reset</PrimaryButton>
+      <PrimaryButton>Confirm</PrimaryButton>
+    </View>
+  );
+}
+
+export default StartGameScreen;
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    marginTop: 100,
+    marginHorizontal: 24,
+    padding: 16,
+    backgroundColor: "#4e0329",
+    borderRadius: 8,
+    // Shadow just for android
+    elevation: 4,
+    // Shadow just for iOS
+    shadowColor: "black",
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 6,
+    shadowOpacity: 0.25,
+  },
+  nameInput: {
+    height: 50,
+    width: 50,
+    fontSize: 32,
+    borderBottomColor: "#ddb52f",
+    borderBottomWidth: 2,
+    color: "#ddb52f",
+    marginVertical: 8,
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+});
