eb3301a49ff5bcf5199ff4bc055fe84c7ce742ff
Martin Aleksandrov <martin.aleksandrov87@gmail.com>
2024-01-10 12:38:00 +0200

    Create title to be reuseable

--- GuessTheNumber/App.js
+++ GuessTheNumber/App.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { StyleSheet, ImageBackground } from "react-native";
+import { StyleSheet, ImageBackground, SafeAreaView } from "react-native";
 import { LinearGradient } from "expo-linear-gradient";
 
 import StartGameScreen from "./screens/StartGameScreen";
@@ -26,7 +26,7 @@ export default function App() {
         style={styles.rootScreen}
         imageStyle={styles.backgroundImage}
       >
-        {screen}
+        <SafeAreaView style={styles.rootScreen}>{screen}</SafeAreaView>
       </ImageBackground>
     </LinearGradient>
   );
@@ -34,7 +34,6 @@ export default function App() {
 
 const styles = StyleSheet.create({
   rootScreen: {
-    backgroundColor: "#ddb52f",
     flex: 1,
   },
   backgroundImage: {
--- GuessTheNumber/screens/GameScreen.js
+++ GuessTheNumber/screens/GameScreen.js
@@ -1,11 +1,25 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
+import Title from "../components/Title";
 
 function GameScreen() {
   return (
-    <View>
-      <Text>Game Screen</Text>
+    <View style={styles.screen}>
+      <Title>Opponent's Guess!</Title>
+      <View>
+        <Text>Higher or lower?</Text>
+      </View>
+      <View>
+        <Text>LOG ROUNDS</Text>
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    padding: 24,
+  },
+});
 
 export default GameScreen;
