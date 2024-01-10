9424faf488b9539e999cb0c153257296082b23d2
Martin Aleksandrov <martin.aleksandrov87@gmail.com>
2024-01-10 11:10:30 +0200

    Added background image

--- GuessTheNumber/App.js
+++ GuessTheNumber/App.js
@@ -1,11 +1,18 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet, ImageBackground } from "react-native";
 import StartGameScreen from "./screens/StartGameScreen";
 import { LinearGradient } from "expo-linear-gradient";
 
 export default function App() {
   return (
     <LinearGradient colors={["#4e0329", "#ddb52f"]} style={styles.rootScreen}>
-      <StartGameScreen />
+      <ImageBackground
+        source={require("./assets/images/background.png")}
+        resizeMode="cover"
+        style={styles.rootScreen}
+        imageStyle={styles.backgroundImage}
+      >
+        <StartGameScreen />
+      </ImageBackground>
     </LinearGradient>
   );
 }
@@ -14,5 +21,8 @@ const styles = StyleSheet.create({
   rootScreen: {
     backgroundColor: "#ddb52f",
     flex: 1,
+  },
+  backgroundImage: {
+    opacity: 0.15,
   },
 });
