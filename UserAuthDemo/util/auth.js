861ed2d1a4ab0150ef3f4673fea747b299c3df67
Martin Aleksandrov <martin.aleksandrov87@gmail.com>
2024-01-24 13:48:58 +0200

    security fixed

--- UserAuthDemo/util/auth.js
+++ UserAuthDemo/util/auth.js
@@ -1,6 +1,5 @@
 import axios from "axios";
-
-const API_KEY = "AIzaSyAzZlg3DuIyuQ-ysMk5fex_KTRDxpTIVJ0";
+import { API_KEY } from "../secrets";
 
 async function authenticate(mode, email, password) {
   const url = `https://identitytoolkit.googleapis.com/v1/accounts:${mode}?key=${API_KEY}`;
