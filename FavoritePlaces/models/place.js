7e21e90cdcdb39fa847b422947f32b81fd6f6852
Martin Aleksandrov <martin.aleksandrov87@gmail.com>
2024-02-04 15:23:07 +0200

    Outputting a List Of Places

--- FavoritePlaces/models/place.js
+++ FavoritePlaces/models/place.js
@@ -1,9 +1,9 @@
-class Place {
-  constructor(title, imageUri, address, location) {
+export class Place {
+  constructor(title, imageUri, location) {
     this.title = title;
     this.imageUri = imageUri;
-    this.address = address;
-    this.location = location; // { lat: 0.115475, lng: 127.214}
+    this.address = location.address;
+    this.location = { lat: location.lat, lng: location.lng }; // { lat: 0.115475, lng: 127.214}
     this.id = new Date().toString() + Math.random().toString();
   }
 }
