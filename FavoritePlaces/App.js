12b8c4e8e4f5784efc0514642f141e9d2bf67da1
Martin Aleksandrov <martin.aleksandrov87@gmail.com>
2024-02-04 19:56:46 +0200

    Adding the Place Details Screen

--- FavoritePlaces/App.js
+++ FavoritePlaces/App.js
@@ -10,6 +10,7 @@ import { useEffect, useState } from "react";
 import { init } from "./util/database";
 import AppLoading from "expo-app-loading";
 import * as SplashScreen from "expo-splash-screen";
+import PlaceDetails from "./screens/PlaceDetails";
 
 const Stack = createNativeStackNavigator();
 
@@ -63,6 +64,7 @@ export default function App() {
             }}
           />
           <Stack.Screen name="Map" component={Map} />
+          <Stack.Screen name="PlaceDetails" component={PlaceDetails} />
         </Stack.Navigator>
       </NavigationContainer>
     </>
--- FavoritePlaces/screens/PlaceDetails.js
+++ FavoritePlaces/screens/PlaceDetails.js
@@ -1,9 +1,49 @@
-import { View, Text } from "react-native";
+import { View, Text, ScrollView, Image, StyleSheet } from "react-native";
+import OutlinedButton from "../components/ui/OutlinedButton";
+import { Colors } from "../constants/colors";
+import { useEffect } from "react";
 
-export default function PlaceDetails() {
+export default function PlaceDetails({ route }) {
+  function showOnMapHandler() {}
+
+  const selectedPlaceId = route.params.placeId;
+
+  useEffect(() => {
+    // use selectedPlaceId to fetch data for a single place
+  }, [selectedPlaceId]);
+
   return (
-    <View>
-      <Text>PlaceDetails</Text>
-    </View>
+    <ScrollView>
+      <Image style={styles.image} />
+      <View style={styles.locationContainer}>
+        <View style={styles.addressContainer}>
+          <Text style={styles.address}>Address</Text>
+        </View>
+        <OutlinedButton icon="map" onPress={showOnMapHandler}>
+          View on Map
+        </OutlinedButton>
+      </View>
+    </ScrollView>
   );
 }
+
+const styles = StyleSheet.create({
+  image: {
+    height: "35%",
+    minHeight: 300,
+    width: "100%",
+  },
+  locationContainer: {
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  addressContainer: {
+    padding: 20,
+  },
+  address: {
+    color: Colors.primary500,
+    textAlign: "center",
+    fontWeight: "bold",
+    fontSize: 16,
+  },
+});
