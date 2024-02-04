cf25dacdde076006bf4bc8abd5a0ce31cb052db9
Martin Aleksandrov <martin.aleksandrov87@gmail.com>
2024-02-04 18:36:25 +0200

    SQLite: Getting Started & Initialization

--- FavoritePlaces/App.js
+++ FavoritePlaces/App.js
@@ -6,10 +6,28 @@ import AddPlace from "./screens/AddPlace";
 import IconButton from "./components/ui/IconButton";
 import { Colors } from "./constants/colors";
 import Map from "./screens/Map";
+import { useEffect, useState } from "react";
+import { init } from "./util/database";
+import AppLoading from "expo-app-loading";
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
+  const [dbInit, setDbInit] = useState(false);
+  useEffect(() => {
+    init()
+      .then(() => {
+        setDbInit(true);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+  if (!dbInit) {
+    return <AppLoading />;
+  }
+
   return (
     <>
       <StatusBar style="dark" />
